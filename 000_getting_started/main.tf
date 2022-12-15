32e4210568c33feaee07f89fe076d0a9951a8c1e
Khalfan Salah <khalfan_salah@epam.com>
2022-12-15 21:12:04 +0100

    install software using cloud-init

--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -11,6 +11,10 @@ locals {
   # subnet_id = "${local.subnet_internal}.id"
 }
 
+data "template_file" "user-data" {
+  template = file("./user-data.yaml")
+}
+
 # Create a resource groups
 resource "azurerm_resource_group" "rg" {
   name     = "${local.resource_group}" 
@@ -70,8 +74,20 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "allow_ssh_sg"
+    name                       = "allow_port_80"
     priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_ssh_sg"
+    priority                   = 101
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -98,6 +114,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   size                = "Standard_F1"
   admin_username      = "adminuser"
   network_interface_ids =  [azurerm_network_interface.nic.id]
+  user_data = base64encode(data.template_file.user-data.rendered)
 
   admin_ssh_key {
     username   = "adminuser"
